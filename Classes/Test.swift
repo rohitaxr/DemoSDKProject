988416bb215e9b1d971c6b71afc3dce75a6bc38b
Rohitax Rajguru <rohitax@svapinfotech.com>
2023-06-27 15:16:07 +0530

    changes.

---
+++ Classes/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  DemoSDKProject
+//
+//  Created by MT11-1219 on 27/06/23.
+//
+
+import Foundation
+import UIKit
+
+class Test {
+    
+    func printFunc() {
+        print("init")
+    }
+}
